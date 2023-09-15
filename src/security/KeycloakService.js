4bdce2c5597740ba14dfbaaef8643d8c6494df5c
Fernando Hara <fernando.hara@segmenta.tec.br>
2023-09-15 16:37:48 -0300

    login

---
+++ src/security/KeycloakService.js
@@ -0,0 +1,26 @@
+import Keycloak from "keycloak-js";
+
+const keycloakInstance = new Keycloak();
+
+/**
+ * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
+ *
+ * @param onAuthenticatedCallback
+ */
+const Login = (onAuthenticatedCallback) => {
+  keycloakInstance
+    .init({ onLoad: "login-required" })
+    .then(function (authenticated) {
+      authenticated ? onAuthenticatedCallback() : alert("non authenticated");
+    })
+    .catch((e) => {
+      console.dir(e);
+      console.log(`keycloak init exception: ${e}`);
+    });
+};
+
+const KeyCloakService = {
+  CallLogin: Login,
+};
+
+export default KeyCloakService;
