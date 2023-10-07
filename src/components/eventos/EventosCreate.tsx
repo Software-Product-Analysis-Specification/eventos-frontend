9fd4dfed7f561380f455d12056a6b0231953cdfb
Fernando Hara <fernando.hara@segmenta.tec.br>
2023-10-07 04:40:51 -0300

    Inclusão de Data

--- src/components/eventos/EventosCreate.tsx
+++ src/components/eventos/EventosCreate.tsx
@@ -1,20 +1,29 @@
 import axios from "axios";
 import { useState } from "react";
+import SemanticDatepicker from "react-semantic-ui-datepickers";
 import { Button, Container, Form } from "semantic-ui-react";
 
 const EventosCreate = () => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-    
+    const [date, setDate] = useState(new Date());
+    const [time, setTime] = useState('');
+
     const postData = () => {
-        axios.post(`https://backend.eventos.fernandohara.com.br/api/eventos`, {
-            "Nome": name,
-            "Descricao": description
-        })
-        .then(() => {
-            setName('');
-            setDescription('');
-        });
+        if(name !== '')
+        {
+            date.setHours(parseInt(time.split(':')[0]), parseInt(time.split(':')[1]), 0);
+            
+            axios.post(`https://backend.eventos.fernandohara.com.br/api/eventos`, {
+                "Nome": name,
+                "Descricao": description,
+                "Data": date
+            })
+            .then(() => {
+                setName('');
+                setDescription('');
+            });
+        }
     };
 
     return (
@@ -29,6 +38,16 @@ const EventosCreate = () => {
                         <label>Descrição</label>
                         <input placeholder='Descrição' value={description} onChange={(e) => setDescription(e.target.value)}/>
                     </Form.Field>
+                    <Form.Group>
+                        <Form.Field>
+                            <label>Data</label>
+                            <SemanticDatepicker locale="pt-BR" format="DD/MM/YYYY" value={date} onChange={(e, data) => setDate(data.value as Date)} />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>Hora</label>
+                            <input type="time" value={time} onChange={(e) => setTime(e.target.value)} />
+                        </Form.Field>
+                    </Form.Group>
                     <Button onClick={postData} type='submit'>Salvar</Button>
                 </Form>
             </Container>
--- src/components/eventos/EventosUpdate.tsx
+++ src/components/eventos/EventosUpdate.tsx
@@ -1,17 +1,34 @@
 import axios from "axios";
+import moment from "moment-timezone";
 import { useEffect, useState } from "react";
+import SemanticDatepicker from "react-semantic-ui-datepickers";
 import { Button, Container, Form } from "semantic-ui-react";
 
 const EventosUpdate = ({ pid, handleId }: { pid: string, handleId: any }) => {
     const [id, setId] = useState('');
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [date, setDate] = useState(new Date());
+    const [time, setTime] = useState('');
 
     useEffect(() => {
-        if(pid !== id)
+        if(pid.toString() !== id.toString())
         {
             axios.get(`https://backend.eventos.fernandohara.com.br/api/eventos/${pid}`)
                 .then((response) => {
+                    if(response.data.data !== null)
+                    {
+                        var gmt = moment.tz(response.data.data, 'GMT');
+                        
+                        setDate(new Date(gmt.tz("America/Sao_Paulo").format('YYYY-MM-DD HH:mm')));
+                        setTime(gmt.tz("America/Sao_Paulo").format('HH:mm'));
+                    }
+                    else
+                    {
+                        setDate(new Date());
+                        setTime('00:00');
+                    }
+
                     setId(response.data.id);
                     setName(response.data.nome);
                     setDescription(response.data.descricao);
@@ -20,10 +37,13 @@ const EventosUpdate = ({ pid, handleId }: { pid: string, handleId: any }) => {
     });
 
     const postData = () => {
+        date.setHours(parseInt(time.split(':')[0]), parseInt(time.split(':')[1]), 0);
+
         axios.put(`https://backend.eventos.fernandohara.com.br/api/eventos`, {
             "Id": id,
             "Nome": name,
-            "Descricao": description
+            "Descricao": description,
+            "Data": date
         })
         .then(() => {
             handleId(undefined);
@@ -46,6 +66,16 @@ const EventosUpdate = ({ pid, handleId }: { pid: string, handleId: any }) => {
                         <label>Descrição</label>
                         <input placeholder='Descrição' value={description} onChange={(e) => setDescription(e.target.value)} />
                     </Form.Field>
+                    <Form.Group>
+                        <Form.Field>
+                            <label>Data</label>
+                            <SemanticDatepicker locale="pt-BR" format="DD/MM/YYYY" value={date} onChange={(e, data) => setDate(data.value as Date)} />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>Hora</label>
+                            <input type="time" value={time} onChange={(e) => setTime(e.target.value)} />
+                        </Form.Field>
+                    </Form.Group>
                     <Button onClick={postData} type='submit'>Salvar</Button><Button onClick={() => handleId(undefined)} type='button'>Cancelar</Button>
                 </Form>
             </Container>
